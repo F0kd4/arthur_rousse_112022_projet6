9c935b9ab5c6c7382be0f85f4286ae7337297dd9
4rty <kingofpunk_edc@hotmail.fr>
2022-11-14 12:23:03 +0100

    implémentation des controllers sauces

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,5 +1,12 @@
 const Sauce = require('../models/Sauce');
 
+
+exports.getAllSauces = (req, res, next) => {
+    Sauce.find()
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(400).json({ error }));
+}
+
 exports.createSauce = (req, res, next) => {
     delete req.body._id;
     const sauce = new Sauce({
@@ -7,5 +14,11 @@ exports.createSauce = (req, res, next) => {
     });
     sauce.save()
         .then(() => res.status(201).json({ message: 'Sauce créée.' }))
+        .catch(error => res.status(400).json({ error }));
+}
+
+exports.deleteSauce = (req, res, next) => {
+    Sauce.deleteOne({ _id: req.params.name })
+        .then(() => res.status(200).json({ message: 'Sauce supprimée.' }))
         .catch(error => res.status(400).json({ error }));
 }
